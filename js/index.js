f22a96484cbd35785f0181719f3452a522200c10
allisonkydy <allisnelly@gmail.com>
2019-08-06 14:12:38 -0500

    added dblclick event listener on nav, reorganized js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,26 @@
 // Your code goes here
 
-// CSS Changes
-document.querySelector('.main-navigation').style.zIndex = 1;
-
+// ------------Selectors------------
+const mainNav = document.querySelector('.main-navigation');
 const images = document.querySelectorAll('img');
+
+// ------------CSS Changes------------
+mainNav.style.zIndex = 1;
+
+// ------------Event Listeners------------
 
 // Fade in images on mouseover
 images.forEach(img => img.addEventListener('mouseover', e => {
     e.target.style.opacity = 0.5;
 }));
 
-// Fade out on mouseleave
+// Fade out images on mouseleave
 images.forEach(img => img.addEventListener('mouseleave', e => {
     e.target.style.opacity = 1;
 }));
 
+// Navigation change color on double click
+mainNav.addEventListener('dblclick', e => {
+    e.target.style.background = '#FFEBCD';
+    console.log('im scrolling');
+});
