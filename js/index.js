19d8226e7c5746c4ae6a2fadfcb1b178d4a01020
allisonkydy <allisnelly@gmail.com>
2019-08-06 13:51:11 -0500

    added mouseover and mouseleave event listeners to images

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+
+const images = document.querySelectorAll('img');
+
+// Fade in images on mouseover
+images.forEach(img => img.addEventListener('mouseover', e => {
+    e.target.style.opacity = 0.5;
+}));
+
+// Fade out on mouseleave
+images.forEach(img => img.addEventListener('mouseleave', e => {
+    e.target.style.opacity = 1;
+}));
