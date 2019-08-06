38c369092c111004d414709fec49b71be48bc5f2
allisonkydy <allisnelly@gmail.com>
2019-08-06 15:03:01 -0500

    added mouseup event to paragraphs

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ const mainNav = document.querySelector('.main-navigation');
 const images = document.querySelectorAll('img');
 const body = document.querySelector('body');
 const text = document.querySelectorAll('p, h2, h4');
+const paragraph = document.querySelectorAll('p');
 
 // ------------CSS Changes------------
 mainNav.style.zIndex = 1;
@@ -38,3 +39,8 @@ body.addEventListener('keyup', e => {
 text.forEach(element => element.addEventListener('click', e => {
     e.target.style.color = 'red';
 }));
+
+// Paragraph size increases on mouseup
+paragraph.forEach(element => element.addEventListener('mouseup', e => {
+    e.target.style.fontSize = '3.6rem';
+}))
